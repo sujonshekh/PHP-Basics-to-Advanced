5ae1b6dc88d158cf7260a9f43e41310e47ee56b6
Sujon Shekh <sj.sujon.shekh@gmail.com>
2022-07-16 14:45:28 +0600

    php basics variables

---
+++ 01 Variables test.php
@@ -0,0 +1,23 @@
+<?php
+// normal variables
+// Single quatation also be in string variables
+$name= 'JHONTHEMAN';
+$age= 31;
+// camelCase also you can't use any space | numeric signs in variables
+$terGet="whole PHP";
+
+echo "Hey Jhon welcome to php, $name, $age","\n";
+echo "\n";
+
+echo "Nice to See you Coding World after so many years, $name, $age","\n";
+echo "$terGet","\n";
+
+// Constant variable always declared with Capital word
+define('PI',3.14159);
+
+// If Contant value declare you need to after quotation thn full stop and call Constant declaration
+echo "value of PI is = ".PI;
+
+// Constant value not to need any of quotation
+echo "\n";
+echo PI;
